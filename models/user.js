7dd9c31c52af19736b0bc4e340663869209758a4
h4tl2 <hat9458g@gmail.com>
2017-11-22 23:00:40 +0700

    hide config

--- models/user.js
+++ models/user.js
@@ -6,7 +6,8 @@ var UserSchema = mongoose.Schema({
 	username: {
 		type: String,
         index:true,
-        required: true
+		required: true,
+		unique : true
 	},
 	password: {
         type: String,
